29e1786098c6ad83e0fa1990fc5ca2884dcff38c
Azfar Hussain <shussain@arcos-inc.com>
2019-04-05 12:56:02 +0500

    Creating Shift Rue Management Classes

---
+++ src/test/java/Steps/RuleSetsSD_ShiftRuleSet.java
@@ -0,0 +1,11 @@
+package Steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RuleSetsSD_ShiftRuleSet {
+
+}
