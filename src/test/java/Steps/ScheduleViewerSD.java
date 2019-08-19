766de53c44bee57d5e59fc5e7e1a3572274f3866
Shahzaib Khan <shahzaibkhan0612@gmail.com>
2019-08-19 16:22:08 +0500

    Implement wait in Schedule Viewer SD file
    --19/08/19

--- src/test/java/Steps/ScheduleViewerSD.java
+++ src/test/java/Steps/ScheduleViewerSD.java
@@ -23,6 +23,7 @@ public class ScheduleViewerSD extends BaseUtil {
 
         System.out.println("Clicking on Schedule viewer");
         scheduleViewerPage.clickOnScheduleViewer();
+        Thread.sleep(1000);
     }
 
     @When("^User is on the Schedule Viewers Screen$")
@@ -36,12 +37,14 @@ public class ScheduleViewerSD extends BaseUtil {
 
         System.out.println("User click on the Add Schedule Viewer to this Location");
         scheduleViewerPage.clickOnTheAddScheduleViewerToThisLocation();
+        Thread.sleep(1000);
     }
 
     @When("^User is on the Find New Schedule Viewer Screen$")
     public void userIsOnTheFindNewScheduleViewerScreen() throws Throwable {
 
         System.out.println("User is on the Find New Schedule Viewer Screen");
+        Thread.sleep(1000);
     }
 
     @And("^User find new schedule by last name \"([^\"]*)\"$")
@@ -49,6 +52,7 @@ public class ScheduleViewerSD extends BaseUtil {
 
         System.out.println("User find new schedule by last name");
         scheduleViewerPage.findByLastName(lastname);
+        Thread.sleep(1000);
     }
 
     @And("^User click on the Search Button$")
@@ -56,6 +60,7 @@ public class ScheduleViewerSD extends BaseUtil {
 
         System.out.println("User click on the Search Button");
         scheduleViewerPage.clickOnSearch();
+        Thread.sleep(1000);
     }
 
     @And("^User select the new schedule viewer$")
@@ -63,6 +68,7 @@ public class ScheduleViewerSD extends BaseUtil {
 
         System.out.println("User select the new schedule viewer");
         scheduleViewerPage.selectNameFromList();
+        Thread.sleep(1000);
     }
 
     @Then("^User click on the Create button$")
@@ -70,6 +76,7 @@ public class ScheduleViewerSD extends BaseUtil {
 
         System.out.println("User click on the Create button");
         scheduleViewerPage.clickOnTheCreateButton();
+        Thread.sleep(1000);
     }
 
     @When("^User click on the Remove Link Text$")
@@ -77,6 +84,7 @@ public class ScheduleViewerSD extends BaseUtil {
 
         System.out.println("User click on the Remove Link Text");
         scheduleViewerPage.removeScheduleViewer();
+        Thread.sleep(1000);
     }
 
     @And("^User click on the OK Button$")
@@ -84,18 +92,21 @@ public class ScheduleViewerSD extends BaseUtil {
 
         System.out.println("User click on the OK Button");
         scheduleViewerPage.clickOKButton();
+        Thread.sleep(1000);
     }
 
     @Then("^User click on the Add Schedule Viewer to Multiple Location$")
     public void userClickOnTheAddScheduleViewerToMultipleLocation() throws Throwable {
 
         scheduleViewerPage.clickOnTheAddScheduleViewerToMultipleLocation();
+        Thread.sleep(1000);
     }
 
     @Then("^User click on the Next button$")
     public void userClickOnTheNextButton() throws Throwable {
 
         scheduleViewerPage.clickNextButton();
+        Thread.sleep(1000);
     }
 
     @When("^User is on the Find New Schedule Viewer Next Screen$")
@@ -108,12 +119,14 @@ public class ScheduleViewerSD extends BaseUtil {
     public void userSelectMultipleLocations() throws Throwable {
 
         scheduleViewerPage.selectLocationsFromList();
+        Thread.sleep(1000);
     }
 
     @When("^User click on the Edit Assignment link text$")
     public void userClickOnTheEditAssignmentLinkText() throws Throwable {
 
         scheduleViewerPage.editAssignmentLink();
+        Thread.sleep(1000);
     }
 
     @Then("^User is on the Edit Schedule Viewer Screen$")
@@ -125,12 +138,14 @@ public class ScheduleViewerSD extends BaseUtil {
     public void userEditLocations() throws Throwable {
 
         scheduleViewerPage.editSchedule();
+        Thread.sleep(1000);
     }
 
     @When("^User click on the Edit Profile link text$")
     public void userClickOnTheEditProfileLinkText() throws Throwable {
 
         scheduleViewerPage.editProfileLink();
+        Thread.sleep(1000);
     }
 
     @Then("^User is on the Edit Profile Viewer Screen$")
@@ -144,11 +159,13 @@ public class ScheduleViewerSD extends BaseUtil {
 
         System.out.println("User click on the Update button");
         scheduleViewerPage.clickOnTheUpdateButton();
+        Thread.sleep(1000);
     }
 
     @And("^User edit Profile \"([^\"]*)\"$")
     public void userEditProfile(String editName) throws Throwable {
 
         scheduleViewerPage.editProfile(editName);
+        Thread.sleep(1000);
     }
 }
