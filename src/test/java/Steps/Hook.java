67309e0cc03efc874a697a2220407f632781987a
Shahzaib Khan <shahzaibkhan0612@gmail.com>
2019-09-13 17:11:41 +0500

    Update Hook.java

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -48,7 +48,7 @@ public class Hook extends BaseUtil {
         ReadExcel.readExcelFile();
     }
 
-    @After("@@ZZZK_Logout")
+    @After("@ZZZK_Logout")
     public void TearDownTest(Scenario scenario) {
 
         if (scenario.isFailed()) {
